6158672dab1c136e17d4be2ad33c29b39fca9dce
Electron24 <luba.xat@mail.ru>
2014-10-11 01:01:30 +0400

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,8 +1,8 @@
+#include "header.h"
 #include <iostream>
 #include <cassert>
 
-#if 0
-// NOTE: you should move this `if` down as you start solving each task
+
 
 void task_1()
 {
@@ -12,15 +12,17 @@ void task_1()
      * pedantic flag here means: strictly as in language standard 
      * for explanation see:
      *  https://gcc.gnu.org/onlinedocs/gcc/Variable-Length.html*/
-    int n = 10;
+    const int n = 10;
     int a[n];
 }
+
 
 // Task 2
 void task_2()
 {
     int a[5] = {0};
     
+    assert(!a[4]);
     //assert(!a[4]) or assert(a[4]) -- place here one of these and
     //                                 explain your choice in commit msg
 }
@@ -31,9 +33,10 @@ int task_3_zero()
     // Task 3
     // Hint 1: fix with initialization (not assignment)
     // Hint 2: you can use only one zero and not five of them! cf. task 2
-    int a[5];
+    int a[5] = {0};
     
     assert(!a[4]);
+
     return a[4]; // don't touch this!
 }
 
@@ -41,8 +44,7 @@ void task_4()
 {
     // Task 4: asserts at the end must hold
     // Hint: fix with initialization (not assignment)
-    int a[5];
-    
+    int a[5] = {1, 2, 3, 4, 5};
     assert(a[0] == 1);
     assert(a[1] == 2);
     assert(a[2] == 3);
@@ -50,33 +52,37 @@ void task_4()
     assert(a[4] == 5);    
 }
 
+
 // Task 5
 // copy arr1 to arr2
 void task_5_copy(int * arr1, int * arr2, int size)
 {
-    arr2 = arr1;
+	for (int i = 0; i < size; i++)
+		arr2[i] = arr1[i];
+    //arr2 = arr1;
 }
 
 // Task 6
 // copy array `arr1` to array `arr2` of the same size
-void task_6_poor_copy(int * arr1, int * arr2)
+void task_6_poor_copy(int * arr1, int * arr2, int size)
 {
     // Hint: something wrong here; test this from main()
-    for(int i = 0; i < sizeof(arr2); ++i)
+    for(int i = 0; i < size; i++)
     {
         arr2[i] = arr1[i];
     }
 }
+
+
 
 // Task 7
 // print array `arr`
 void task_7_print(int * arr, int size, char delim = ' ')
 {
     // Hint: something wrong here; run this from main()
-    for(int i = 0; i <= size; ++i)
-    {
-        std::cout << a[i] << delim;
-    }
+    for(int i = 0; i < size; i++)
+        std::cout << arr[i] << delim;
 }
 
+#if 0
 #endif
